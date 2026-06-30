47ad92b389f83f3ddc37dc1eb47e2ea086a22eb4
Cristiandis <izzocristian03@gmail.com>
2026-06-30 09:59:10 +0200

    add .desktop file

--- pkgs/questpatcher/default.nix
+++ pkgs/questpatcher/default.nix
@@ -3,6 +3,7 @@
   buildDotnetModule,
   dotnetCorePackages,
   fetchFromGitHub,
+  fetchurl,
   libX11,
   libGL,
   fontconfig,
@@ -12,6 +13,7 @@
   bash,
   android-tools,
   makeWrapper,
+  python3,
 }:
 
 buildDotnetModule rec {
@@ -21,8 +23,13 @@ buildDotnetModule rec {
   src = fetchFromGitHub {
     owner = "Lauriethefish";
     repo = "QuestPatcher";
-    rev = "2.10.0";
+    rev = version;
     hash = "sha256-ed/4AH+kNmDbDd5zzaNA6tIwHdliv/isJaNGwuOsMsk=";
+  };
+
+  icon = fetchurl {
+    url = "https://raw.githubusercontent.com/Lauriethefish/QuestPatcher/${version}/QuestPatcher/Assets/questpatcher-logo.ico";
+    hash = "sha256-koOqmch4dmWZ4Uh21P3eZQQRE7onYjXngFBHQPnECQ8=";
   };
 
   projectFile = "QuestPatcher/QuestPatcher.csproj";
@@ -30,6 +37,11 @@ buildDotnetModule rec {
 
   dotnet-sdk = dotnetCorePackages.sdk_6_0;
   dotnet-runtime = dotnetCorePackages.runtime_6_0;
+
+  nativeBuildInputs = [
+    makeWrapper
+    (python3.withPackages (ps: [ ps.pillow ]))
+  ];
 
   runtimeDeps = [
     libX11
@@ -48,6 +60,25 @@ buildDotnetModule rec {
       ]
     }"
   ];
+
+  postInstall = ''
+    python3 -c "
+    from PIL import Image
+    img = Image.open('${icon}')
+    img.save('questpatcher.png', format='PNG', sizes=[(256,256)])
+    "
+    install -Dm644 questpatcher.png $out/share/icons/hicolor/256x256/apps/questpatcher.png
+    mkdir -p $out/share/applications
+    cat > $out/share/applications/questpatcher.desktop <<EOF
+    [Desktop Entry]
+    Name=QuestPatcher
+    Comment=Generic il2cpp modding tool for Oculus Quest apps
+    Exec=QuestPatcher
+    Icon=questpatcher
+    Type=Application
+    Categories=Utility;
+    EOF
+  '';
 
   executables = [ "QuestPatcher" ];
 
